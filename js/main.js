636975f7501da64fb9acee64acbde4246a38f292
Tomas Gabriel Luna <tomasgabriel1999@hotmail.com>
2025-06-08 10:30:34 -0300

    realizamos la logica para los tipos de pokemones

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ for (let i = 1; i <= 151; i++){
 }
 
 function mostrarPokemon(poke) {
+
+    let tipos = poke.types.map(type => 
+        ` <p class="${type.type.name} tipo">${type.type.name}</p>`);
+        tipos = tipos.join("")
+
     const div = document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML = ` <p class="pokemon-id-back">#${poke.id}</p>
@@ -20,8 +25,7 @@ function mostrarPokemon(poke) {
                             <h2 class="pokemon-nombre">${poke.name}</h2>
                         </div>
                         <div class="pokemon-tipos">
-                            <p class="electric tipo">ELECTRIC</p>
-                            <p class="fighting tipo">FIGHTING</p>
+                            ${tipos}
                         </div>
                         <div class="pokemon-stats">
                             <p class="stat">${poke.height}</p>
