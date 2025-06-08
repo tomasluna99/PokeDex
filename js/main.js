80e0da0d531fa32446f58669f383ee69f25e1c9b
Tomas Gabriel Luna <tomasgabriel1999@hotmail.com>
2025-06-08 10:36:35 -0300

    agregamos los numeros a cada pokemon

--- js/main.js
+++ js/main.js
@@ -11,17 +11,24 @@ function mostrarPokemon(poke) {
 
     let tipos = poke.types.map(type => 
         ` <p class="${type.type.name} tipo">${type.type.name}</p>`);
-        tipos = tipos.join("")
+        tipos = tipos.join("");
+
+        let pokeId = poke.id.toString();
+        if (pokeId.length === 1 ) {
+            pokeId = "00" + pokeId;
+        } else if (pokeId.length === 2){
+            pokeId = "0" + pokeId;
+        }
 
     const div = document.createElement("div");
     div.classList.add("pokemon");
-    div.innerHTML = ` <p class="pokemon-id-back">#${poke.id}</p>
+    div.innerHTML = ` <p class="pokemon-id-back">#${pokeId}</p>
                     <div class="pokemon-imagen">
                         <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
                     </div>
                     <div class="pokemon-info">
                         <div class="nombre-contenedor">
-                            <p class="pokemon-id">#${poke.id}</p>
+                            <p class="pokemon-id">#${pokeId}</p>
                             <h2 class="pokemon-nombre">${poke.name}</h2>
                         </div>
                         <div class="pokemon-tipos">
