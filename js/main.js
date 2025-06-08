e83a234c2a2203b71247ef0f30409448911f1786
Tomas Gabriel Luna <tomasgabriel1999@hotmail.com>
2025-06-08 10:50:33 -0300

    concluimos la pagina haciendo que cada boton tenga su funcion

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/"
 
 for (let i = 1; i <= 151; i++){
@@ -42,25 +43,28 @@ function mostrarPokemon(poke) {
                         listaPokemon.append(div);
 }
 
-/* 
-<div class="pokemon">
-                    <p class="pokemon-id-back">#025</p>
-                    <div class="pokemon-imagen">
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu">
-                    </div>
-                    <div class="pokemon-info">
-                        <div class="nombre-contenedor">
-                            <p class="pokemon-id">#025</p>
-                            <h2 class="pokemon-nombre">Pikachu</h2>
-                        </div>
-                        <div class="pokemon-tipos">
-                            <p class="electric tipo">ELECTRIC</p>
-                            <p class="fighting tipo">FIGHTING</p>
-                        </div>
-                        <div class="pokemon-stats">
-                            <p class="stat">4m</p>
-                            <p class="peso">60kg</p>
-                        </div>
-                    </div>
+botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+    const botonId = event.currentTarget.id;
+    
+    listaPokemon.innerHTML = "";
 
-*/
+    for (let i = 1; i <= 151; i++){
+     fetch(URL + i)
+     .then((response) => response.json())
+     .then(data => {
+
+        if(botonId === "ver-todos"){
+            mostrarPokemon(data);
+        } else{
+            
+        const tipos = data.types.map(type => type.type.name);
+        if(tipos.some(tipo => tipo.includes(botonId))) {
+            mostrarPokemon(data);
+        }
+        
+        }
+
+        
+     })
+}
+}))
